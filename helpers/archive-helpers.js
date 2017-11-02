4ac800ee42d7dee5cbf2306e2dad381cb97ab3d6
mbntex <mbneveu@gmail.com>
2017-11-01 20:08:01 -0700

    active server returns static pages and url query workign on nested callbacks

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 
+
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
@@ -26,9 +27,31 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  // fs.readFile('./archives/sites.txt', 'utf8', function (err, data) {
+  //   if (err) { console.log('FILE READ ERROR =', err); }
+  //   return callback(data);
+  // });
+  fs.read('./archives/sites.txt', 'utf8', function (err, data) {
+    if (err) { console.log('ERROR', error); }
+    return callback(data);
+  });
 };
 
+
+
+
 exports.isUrlInList = function(url, callback) {
+  
+  
+//   var rawSiteListData;
+//   var cb = function (item) { test = rawSiteListData; console.log ('rawSiteListData = ', rawSiteListData); }; 
+//   exports.readListOfUrls(cb);
+
+// //////////////////////////////////
+//   var rawSiteListData = exports.readListOfUrls(function (item) { return item; });
+//   console.log('rawSiteListData =', rawSiteListData);  
+// /////////////////////////////////
+  
 };
 
 exports.addUrlToList = function(url, callback) {
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,85 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers.js');
+var fs = require('fs');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('request handler ran');
+  console.log('REQ URL', req.url);
+
+  if (req.method === 'GET' && req.url === '/') {
+    //console.log('URL = ', req.url);
+    console.log('TEST456');
+    fs.readFile('./web/public/index.html', 'utf8', function (err, data) {
+      if (err) {
+        console.log('ERROR = ', err);
+      }
+      // console.log('DATA RECIEVED = ', data);
+      // console.log('TYPE OF DATA RECIEVED = ', typeof data);
+      status = 200;
+      res.writeHead(status, helpers.headers);
+      res.end(data);
+      
+    
+    });
+  } 
+
+  if (req.method === 'GET' && req.url === '/loading.html') {
+    //console.log('URL = ', req.url);
+    console.log('TEST456');
+    fs.readFile('./web/public/loading.html', 'utf8', function (err, data) {
+      if (err) {
+        console.log('ERROR = ', err);
+      }
+      // console.log('DATA RECIEVED = ', data);
+      // console.log('TYPE OF DATA RECIEVED = ', typeof data);
+      status = 200;
+      res.writeHead(status, helpers.headers);
+      res.end(data);
+    });
+  } 
+
+  if (req.method === 'POST') {
+    console.log('post attempted');
+    var body;
+    req.on('data', function (chunk) { body += chunk; });
+    req.on('end', function () {
+      status = 201;
+      res.writeHead(status, helpers.headers);
+      var urlWanted = body.substring((body.indexOf('url=') + 4), body.length);
+      
+      
+
+      // archive.readListOfUrls(function(thing) { console.log('DATA == ', thing); console.log('DATA TYPE == ', typeof thing); } );
+      
+      //archive.readListOfUrls(function(thing) {   });
+
+      archive.isUrlInList();
+
+
+
+
+
+
+
+
+      res.end(urlWanted);
+    });   
+  }
+
+
+
+
+  if (req.method === 'GET' && (req.url !== '/' && req.url !== '/loading.html')) {
+    status = 200;
+    res.writeHead(status, helpers.headers);
+    res.end('Other Page');
+    console.log('GET REQ OTHER PAGE');
+  }
+
+  // res.end(archive.paths.list);
 };
+
+//public/index.html'
